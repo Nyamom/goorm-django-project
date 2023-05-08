5bbee87bde6b62f970fbef7c78f71259e73a30a0
Nyamom <wjdghxp@gmail.com>
2023-05-08 07:56:27 +0000

    commit

--- projects/mysite/pybo/models.py
+++ projects/mysite/pybo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,7 +8,8 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.subject
 
@@ -16,6 +18,8 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.content
+
