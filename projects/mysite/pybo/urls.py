4be7d2c4a2c72314f000a2efeaa48ab06332407c
Nyamom <wjdghxp@gmail.com>
2023-05-10 01:39:48 +0000

    commit

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('answer/create/<int:question_id>/', views.answer_create,
          name='answer_create'),
     path('question/creat/', views.question_create, name='question_create'),
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
 ]
